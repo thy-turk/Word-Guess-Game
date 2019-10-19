497c334a266ed0daf8afe3712707567e235b0076
Turk <tuckerfiumara@gmail.com>
2019-10-19 15:29:48 -0700

    Added work to JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+//  // Array for the word bank of possible answers
+//  var wordAnswers = ["vapor", "wave", "test", "keyboard"];
+
+//  // Math function to randomly pick a word from the wordbank
+//  var answer = wordAnswers[Math.floor(Math.random()*wordAnswers.length)];
+
+//  // Variable that counts the number of guesses left
+//  var guessesLeft = 15;
+//  document.getElementById("guesses-remain").innerHTML = guessesLeft;
+
+//  // Variable that counts the number of wins
+//  var wins = 0;
+//  document.getElementById("num-of-wins").innerHTML = wins;
+ 
+//  var currentWord = [];
+//  var i;
+ 
+//  // Loop that creates empty spaces for the words
+//  for (i = 0; i < answer.length; i++) {
+//      currentWord.push("__");
+//  }
+//  document.getElementById("active-word").innerHTML = currentWord.join(" ");
+
+ 
+
+// function newFunction() {
+//     return 15;
+// }
+
